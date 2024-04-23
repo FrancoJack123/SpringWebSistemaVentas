721016c6f88ba8f266be402516132b92caf1d803
FrancoJack123 <jackfrancomoreno15@gmail.com>
2024-04-22 20:33:53 -0500

    first commit

---
+++ src/main/java/pe/edu/cibertec/springwebsistemaventas/SpringWebSistemaVentasApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.springwebsistemaventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSistemaVentasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebSistemaVentasApplication.class, args);
+    }
+
+}
